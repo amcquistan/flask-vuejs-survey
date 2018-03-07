9f69e615e59277dc74f460b369b0699d2ee25eef
Adam McQuistan <sci.guy.mcq@gmail.com>
2018-03-06 22:15:49 -0600

    Fourth post backend complete

---
+++ backend/appserver.py
@@ -0,0 +1,9 @@
+"""
+appserver.py
+- creates an application instance and runs the dev server
+"""
+
+if __name__ == '__main__':
+  from surveyapi.application import create_app
+  app = create_app()
+  app.run()
---
+++ backend/surveyapi/application.py
@@ -0,0 +1,18 @@
+"""
+application.py
+- creates a Flask app instance and registers the database object
+"""
+
+from flask import Flask
+
+def create_app(app_name='SURVEY_API'):
+  app = Flask(app_name)
+  app.config.from_object('surveyapi.config.BaseConfig')
+
+  from surveyapi.api import api
+  app.register_blueprint(api, url_prefix="/api")
+
+  from surveyapi.models import db
+  db.init_app(app)
+
+  return app
